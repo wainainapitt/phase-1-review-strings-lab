d6adc29124cb8d3574e56cae059472660376e86c
peter <pngugi.wainaina@gmail.com>
2023-10-01 20:10:47 +0300

    strings lab done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+
+const currentUser = "Peter Wainaina";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+//exctedWelcome message
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+//shortGreeting"Welcome, ";
+ const shortGreeting = `Welcome, ${currentUser[0]}!`;
+
